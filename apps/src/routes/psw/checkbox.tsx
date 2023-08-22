06bd7cae29580e9c8aed3ddffafe35e46f730a2c
BorisKlco <klco@pm.me>
2023-08-22 23:09:24 +0200

    psw generation

--- apps/src/routes/psw/checkbox.tsx
+++ apps/src/routes/psw/checkbox.tsx
@@ -1,8 +1,31 @@
-export default function Checkbox({ text }: { text: string }) {
+import { SettingType } from "./psw";
+
+type CheckboxType = {
+  text: string;
+  checkType: string;
+  checkState: SettingType;
+  checkSetter: React.Dispatch<React.SetStateAction<SettingType>>;
+};
+
+export default function Checkbox({
+  text,
+  checkType,
+  checkState,
+  checkSetter,
+}: CheckboxType) {
+  const handleCheck = () => {
+    checkSetter({ ...checkState, [checkType]: !checkState[checkType] });
+  };
   return (
     <span className="flex items-center gap-3 text-md">
       <div className="ring-2 ring-violet-400 h-5 hover:bg-violet-500">
-        <input className="accent-black opacity-40 w-5 h-5" type="checkbox" />
+        <input
+          checked={checkState[checkType]}
+          onChange={handleCheck}
+          className="accent-black opacity-40 w-5 h-5"
+          type="checkbox"
+          name={checkType}
+        />
       </div>
       {text}
     </span>
--- apps/src/routes/psw/psw.tsx
+++ apps/src/routes/psw/psw.tsx
@@ -1,12 +1,50 @@
 import { MdContentCopy } from "react-icons/md";
 import { useState } from "react";
 import Checkbox from "./checkbox";
-export default function psw() {
-  const [psw, setPsw] = useState("ZKdi!nJ7%U^PZM");
-  const [len, setLen] = useState(8);
+
+export type SettingType = {
+  [key: string]: boolean;
+};
+
+export default function PswGenerator() {
+  const [psw, setPsw] = useState("");
+  const [userLen, setLen] = useState(8);
+  const [sett, setSett] = useState<SettingType>({
+    up: true,
+    low: true,
+    num: true,
+    sym: true,
+  });
 
   const handleCopyPsw = () => {
     navigator.clipboard.writeText(psw);
+  };
+
+  const handleGenerate = () => {
+    //User choice control
+    const empty = Object.keys(sett).filter((item) => sett[item] != false);
+    if (!empty.length) {
+      return setPsw("Pick Setting");
+    }
+    //Generate password
+    const dict: { [key: string]: string } = {
+      up: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      low: "abcdefghijklmnopqrstuvwxyz",
+      num: "0123456789",
+      sym: "!@#$%^&*()_+-={}[]|;:,.<>?/~",
+    };
+    const settKeys = Object.keys(sett);
+    let password = "";
+    while (password.length < userLen) {
+      const key = Math.floor(Math.random() * settKeys.length);
+      if (sett[settKeys[key]]) {
+        const randomKey = Math.floor(
+          Math.random() * dict[settKeys[key]].length
+        );
+        password += dict[settKeys[key]][randomKey];
+      }
+    }
+    setPsw(password);
   };
 
   return (
@@ -25,7 +63,7 @@ export default function psw() {
         <div className="text-white px-6 py-4 bg-gray-800/80 select-none">
           <div className="flex items-center justify-between">
             <p className="text-xl">Character length</p>
-            <span className="text-violet-400 text-2xl">{len}</span>
+            <span className="text-violet-400 text-2xl">{userLen}</span>
           </div>
           <div className="flex flex-col gap-4 items-start mt-4">
             <input
@@ -33,19 +71,42 @@ export default function psw() {
               type="range"
               min="8"
               max="16"
-              value={len}
+              value={userLen}
               onChange={(e) => setLen(+e.target.value)}
             />
-            <Checkbox text="Include Uppercase" />
-            <Checkbox text="Include Lowercase" />
-            <Checkbox text="Include Numbers" />
-            <Checkbox text="Include Symbols" />
+            <Checkbox
+              text="Include Uppercase"
+              checkType="up"
+              checkState={sett}
+              checkSetter={setSett}
+            />
+            <Checkbox
+              text="Include Lowercase"
+              checkType="low"
+              checkState={sett}
+              checkSetter={setSett}
+            />
+            <Checkbox
+              text="Include Numbers"
+              checkType="num"
+              checkState={sett}
+              checkSetter={setSett}
+            />
+            <Checkbox
+              text="Include Symbols"
+              checkType="sym"
+              checkState={sett}
+              checkSetter={setSett}
+            />
           </div>
           <div className="flex justify-between w-full px-6 py-4 mt-6 bg-slate-900">
             <h1 className="text-gray-400">STRENGTH</h1>
             <span>||||</span>
           </div>
-          <button className="w-full text-slate-950 font-bold py-4 mt-4 text-center bg-violet-400 transition hover:bg-violet-500">
+          <button
+            onClick={handleGenerate}
+            className="w-full text-slate-950 font-bold py-4 mt-4 text-center bg-violet-400 transition hover:bg-violet-500"
+          >
             GENERATE
           </button>
         </div>
