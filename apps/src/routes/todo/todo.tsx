9a84f994156c997f0d5aaa85b93abbf8f5a0e1e0
BorisKlco <klco@pm.me>
2023-08-22 11:51:09 +0200

    bot bar

--- apps/src/routes/todo/todo.tsx
+++ apps/src/routes/todo/todo.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CgRadioCheck } from "react-icons/cg";
 import { LiaCheckCircle } from "react-icons/lia";
+import { HiMiniArrowsUpDown } from "react-icons/hi2";
 
 type TodosType = {
   todo: string;
@@ -9,10 +10,11 @@ type TodosType = {
 export default function Todo() {
   const [todo, addToDo] = useState("");
   const [tasks, setTasks] = useState<TodosType>([]);
+  const [selectTasks, setSelectTasks] = useState<boolean | null>(null);
 
   const handleAddTask = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      setTasks([...tasks, { todo: event.currentTarget.value, done: false }]);
+      setTasks([{ todo: event.currentTarget.value, done: false }, ...tasks]);
       addToDo("");
     }
   };
@@ -27,13 +29,28 @@ export default function Todo() {
     setTasks(updateTask);
   };
 
+  const handleReverseList = () => {
+    const reverseList = [...tasks].reverse();
+    setTasks(reverseList);
+  };
+
+  console.log(tasks);
+
   return (
     <div className="h-full bg-violet-950 flex justify-center pt-12">
       <div className="p-2 min-w-[20rem] md:min-w-[32rem] flex flex-col gap-4">
-        <div className="">
+        <div className="flex justify-between">
           <h1 className="font-semibold text-4xl text-white tracking-[0.75rem]">
             TODO
           </h1>
+          {tasks.length > 1 && (
+            <div
+              className="flex items-center text-white"
+              onClick={handleReverseList}
+            >
+              <HiMiniArrowsUpDown size={36} className="cursor-pointer" />
+            </div>
+          )}
         </div>
         <div className="flex items-center rounded bg-slate-900/60 text-white pl-2 py-2 gap-2 drop-shadow-lg">
           <CgRadioCheck className="opacity-40" size={20} />
@@ -47,27 +64,50 @@ export default function Todo() {
           />
         </div>
         <div className="rounded bg-slate-900/60 text-gray-300 drop-shadow-lg">
-          {tasks.map((item, i) => (
-            <div
-              onClick={() => handleDoneTask(i)}
-              key={i}
-              className={`flex items-center gap-2 py-2 border-b-[1px] border-gray-400/20 px-2 transition hover:backdrop-contrast-[1.05] ${
-                item.done && "line-through"
-              }`}
-            >
-              {item.done ? (
-                <LiaCheckCircle className="opacity-40" size={20} />
-              ) : (
-                <CgRadioCheck className="opacity-40" size={20} />
-              )}
-              <span>{item.todo}</span>
-            </div>
-          ))}
+          {selectTasks === null
+            ? tasks.map((item, i) => (
+                <div
+                  onClick={() => handleDoneTask(i)}
+                  key={i}
+                  className={`flex items-center gap-2 py-2 border-b-[1px] border-gray-400/20 px-2 transition hover:backdrop-contrast-[1.05] ${
+                    item.done && "line-through opacity-50 blur-[0.5px]"
+                  }`}
+                >
+                  {item.done ? (
+                    <LiaCheckCircle size={20} />
+                  ) : (
+                    <CgRadioCheck className="opacity-40" size={20} />
+                  )}
+                  <span>{item.todo}</span>
+                </div>
+              ))
+            : selectTasks
+            ? "True"
+            : "false"}
 
-          <div className="flex justify-between p-2 text-[0.5rem] text-gray-300/50">
-            <p>left text</p>
-            <div>middle</div>
-            <p>right text</p>
+          <div className="flex justify-between p-2 text-sm text-gray-300/50">
+            <p>{tasks.length} Items</p>
+            <div className="flex gap-2">
+              <button
+                onClick={() => setSelectTasks(null)}
+                className={`${selectTasks === null && "text-violet-300"}`}
+              >
+                All
+              </button>
+              <button
+                onClick={() => setSelectTasks(true)}
+                className={`${selectTasks === true && "text-violet-300"}`}
+              >
+                Active
+              </button>
+              <button
+                onClick={() => setSelectTasks(false)}
+                className={`${selectTasks === false && "text-violet-300"}`}
+              >
+                Completed
+              </button>
+            </div>
+            <button onClick={() => setTasks([])}>Clear</button>
           </div>
         </div>
       </div>
