0c7efcb33a5eaddab58175cc9ad4a4d4171e9b68
Boris Klco <klco@pm.me>
2023-08-29 21:14:16 +0200

    user logic

--- movies/app/routes/_auth.login.tsx
+++ movies/app/routes/_auth.login.tsx
@@ -1,14 +1,29 @@
 import { ActionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
 
 export async function action({ request }: ActionArgs) {
   const body = await request.formData();
-  console.log(body.get("name"), body.get("psw"));
+  try {
+    const login = await db.user.findUniqueOrThrow({
+      where: {
+        user: body.get("name") as string,
+      },
+    });
 
-  return { ok: "ok" };
+    if (login.psw === body.get("psw")) {
+      return { status: "ok" };
+    }
+  } catch (error) {
+    return { error: "Wrong username" };
+  }
+
+  return { error: "Wrong password" };
 }
 
 export default function Login() {
+  const resp = useActionData();
   return (
     <>
       <div className="grid place-items-start justify-center sm:place-content-center h-full">
@@ -21,9 +36,13 @@ export default function Login() {
             <label htmlFor="name" className="w-full flex justify-between">
               Name <input type="text" name="name" className="" />
             </label>
+
             <label htmlFor="password" className="w-full flex justify-between">
               Psw <input type="password" name="psw" />
             </label>
+            {resp && resp.error ? (
+              <span className="text-red-900 text-semibold">{resp.error}</span>
+            ) : null}
 
             <button className="mt-4 py-2 px-4 border border-black">
               Login
