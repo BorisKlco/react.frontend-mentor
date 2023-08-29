96436f48f69de9b189ce3f1a57537dc17dfc1c0c
Boris Klco <klco@pm.me>
2023-08-29 20:44:48 +0200

    registration

--- movies/app/routes/_auth.register.tsx
+++ movies/app/routes/_auth.register.tsx
@@ -1,3 +1,44 @@
+import { ActionArgs, json } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export async function action({ request }: ActionArgs) {
+  const body = await request.formData();
+
+  const reg = await db.user.create({
+    data: {
+      user: body.get("name") as string,
+      psw: body.get("psw") as string,
+      cookie: "123",
+    },
+  });
+
+  return json({ reg });
+}
+
 export default function Register() {
-  return "register";
+  return (
+    <>
+      <div className="grid place-items-start justify-center sm:place-content-center h-full">
+        <div className="py-4 px-6 bg-slate-400 h-auto w-auto">
+          <Form
+            method="post"
+            action="/register"
+            className="flex flex-col gap-4 w-[18rem]"
+          >
+            <label htmlFor="name" className="w-full flex justify-between">
+              Name <input type="text" name="name" className="" />
+            </label>
+            <label htmlFor="password" className="w-full flex justify-between">
+              Psw <input type="password" name="psw" />
+            </label>
+
+            <button className="mt-4 py-2 px-4 border border-black">
+              Register
+            </button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 }
